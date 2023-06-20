c45160fefd5f4e7deb757ba60a32df7b2025d165
Konstantinos Chaldaiopoulos <kostaschaldaio@gmail.com>
2023-06-21 01:04:55 +0300

    Update models.py

--- models.py
+++ models.py
@@ -10,27 +10,28 @@ import itertools
 from torchvision.transforms import Compose, Resize, ToTensor
 
 class UNetGenerator(nn.Module):
-    def __init__(self, in_channels=3, out_channels=3, num_filters=128):
+    def __init__(self, in_channels=3, out_channels=3, num_filters=64):
         super(UNetGenerator, self).__init__()
 
 
-        self.enc1 = self.encoder_block(in_channels, num_filters, normalize=False)
-        self.enc2 = self.encoder_block(num_filters, num_filters * 2)
-        self.enc3 = self.encoder_block(num_filters * 2, num_filters * 4)
-        self.enc4 = self.encoder_block(num_filters * 4, num_filters * 8)
+        self.enc1 = self.encoder_block(in_channels, num_filters*2, normalize=True)
+        self.enc2 = self.encoder_block(num_filters*2, num_filters * 4)
+        self.enc3 = self.encoder_block(num_filters * 4, num_filters * 8)
+        self.enc4 = self.encoder_block(num_filters * 8, num_filters * 16)
 
 
         self.bottleneck = nn.Sequential(
-            nn.Conv2d(num_filters * 8, num_filters * 8, kernel_size=3, padding=1),
-            nn.InstanceNorm2d(num_filters * 8),
-            nn.ReLU(inplace=True)
+            nn.ConvTranspose2d(num_filters * 16, num_filters * 16, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(num_filters * 16),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5)
         )
 
-        self.dec4 = self.decoder_block(num_filters * 16, num_filters * 4)
-        self.dec3 = self.decoder_block(num_filters * 8, num_filters * 2)
-        self.dec2 = self.decoder_block(num_filters * 4, num_filters)
+        self.dec4 = self.decoder_block(num_filters * 32, num_filters * 8, dropout = True)
+        self.dec3 = self.decoder_block(num_filters * 16, num_filters * 4, dropout = True)
+        self.dec2 = self.decoder_block(num_filters * 8, num_filters*2)
         self.dec1 = nn.Sequential(
-            nn.ConvTranspose2d(num_filters * 2, out_channels, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(num_filters * 4, out_channels, kernel_size=4, stride=2, padding=1),
             nn.Tanh()
         )
     def encoder_block(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1, normalize=True):
@@ -40,13 +41,17 @@ class UNetGenerator(nn.Module):
         layers.append(nn.LeakyReLU(0.2, inplace=True))
         return nn.Sequential(*layers)
 
-    def decoder_block(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1):
-      layers = [
-          nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
-          nn.InstanceNorm2d(out_channels),
-          nn.ReLU(inplace=True)
-      ]
-      return nn.Sequential(*layers)
+    def decoder_block(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1, dropout=False):
+        layers = [
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        ]
+
+        if dropout:
+            layers.append(nn.Dropout(0.5))
+
+        return nn.Sequential(*layers)
 
     def forward(self, x):
 
@@ -54,22 +59,52 @@ class UNetGenerator(nn.Module):
 
 
         enc1 = self.enc1(x)
+
         enc2 = self.enc2(enc1)
+
         enc3 = self.enc3(enc2)
+
         enc4 = self.enc4(enc3)
 
 
-        bottleneck = self.bottleneck(enc4)
+
+        bottleneck = self.bottleneck(enc4) # dec 5
 
 
         dec4 = self.dec4(torch.cat((bottleneck, enc4), dim=1))
+
         dec3 = self.dec3(torch.cat((dec4, enc3), dim=1))
+
         dec2 = self.dec2(torch.cat((dec3, enc2), dim=1))
+
         out = self.dec1(torch.cat((dec2, enc1), dim=1))
+
 
 
         out = F.interpolate(out, size=(140, 250))
         return out
+
+class PatchGANDiscriminator(nn.Module):
+    def __init__(self, in_channels=6, num_filters=128):
+        super(PatchGANDiscriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            self.disc_block(in_channels, num_filters, normalize=False),
+            self.disc_block(num_filters, num_filters * 2),
+            self.disc_block(num_filters * 2, num_filters * 4),
+            self.disc_block(num_filters * 4, num_filters * 8, stride=1),
+            nn.Conv2d(num_filters * 8, 1, kernel_size=3, padding=1)
+        )
+
+    def disc_block(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1, normalize=True):
+        layers = [nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)]
+        if normalize:
+            layers.append(nn.InstanceNorm2d(out_channels))
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
 
 class PatchGANDiscriminator(nn.Module):
     def __init__(self, in_channels=6, num_filters=128):
